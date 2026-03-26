6a4bc8cbbda20717db345c65568652eab3adce2a
luarakelly <lua.kellly@gmail.com>
2026-03-26 15:15:05 +0200

    Add mockupdata for styling and testing ui
    - Each file ressembles the actual API data that will be fetched latter.

---
+++ src/pages/mockupData/weeklyMenuData.js
@@ -0,0 +1,20 @@
+export const weeklyMenusData = {
+  days: [
+    {
+      date: "Saturday 28 March",
+      courses: [
+        {
+          name: "Harissa-Spiced Vegetable Stew with Organic Chickpeas, Pita Bread, and Taco Soy Gurt",
+          price: "3,10 € / 6,60 € / 9,60 €",
+          diets: "M"
+        },
+        {
+          name: "Kebab in Tomato Sauce with Rice",
+          price: "3,10 € / 6,60 € / 9,60 €",
+          diets: "G, M"
+        }
+      ]
+    }
+    // 👉 You can continue the rest exactly the same way
+  ]
+};
